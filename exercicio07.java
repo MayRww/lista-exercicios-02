fdaa4c25e7500cb7ff417969514a0b2efe590971
Mayara <mayara.wojceikovski@gmail.com>
2024-03-22 18:14:04 -0300

    exercicio07

---
+++ exercicio07.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class exercicio07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final int TAMANHO = 5;
+        int[] vetor1 = new int[TAMANHO];
+        int[] vetor2 = new int[TAMANHO];
+
+        System.out.println("Digite os elementos do primeiro vetor:");
+        for (int i = 0; i < TAMANHO; i++) {
+            vetor1[i] = scanner.nextInt();
+        }
+
+        System.out.println("Digite os elementos do segundo vetor:");
+        for (int i = 0; i < TAMANHO; i++) {
+            vetor2[i] = scanner.nextInt();
+        }
+
+        int i;
+        for (i = 0; i < TAMANHO; i++) {
+            if (vetor1[i] != vetor2[i]) {
+                System.out.println("Os vetores são diferentes.");
+                break;
+            }
+        }
+
+        if (i == TAMANHO) {
+            System.out.println("Os vetores são iguais.");
+        }
+
+        scanner.close();
+    }
+}
