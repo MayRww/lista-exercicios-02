0e66233d5f3cbb94533a9e87057b4efef4b8e87b
Mayara <mayara.wojceikovski@gmail.com>
2024-03-22 18:32:52 -0300

    exercicio10

---
+++ exercicio10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exercicio10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final int TAMANHO = 5;
+        int[] vetorA = new int[TAMANHO];
+        int[] vetorB = new int[TAMANHO];
+        System.out.println("Digite os elementos do vetor A:");
+        for (int i = 0; i < TAMANHO; i++) {
+            vetorA[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < TAMANHO; i++) {
+            vetorB[i] = vetorA[TAMANHO - 1 - i];
+        }
+
+        System.out.println("Vetor B (cópia invertida de A):");
+        for (int i = 0; i < TAMANHO; i++) {
+            System.out.print(vetorB[i] + " ");
+        }
+        System.out.println();
+
+        scanner.close();
+    }
+}
