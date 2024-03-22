df650fd9f5a60331c253489c5cc9ba18669c0a51
Mayara <mayara.wojceikovski@gmail.com>
2024-03-21 22:47:55 -0300

    exercicio03

---
+++ exercicio03.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class exercicio03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+           System.out.print("Digite o tamanho do vetor: ");
+        int tamanho = scanner.nextInt();
+
+          int[] vetorOriginal = new int[tamanho];
+         System.out.println("Digite os números do vetor:");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print("Digite o número na posição " + i + ": ");
+            vetorOriginal[i] = scanner.nextInt();
+        }
+
+         int[] vetorDobro = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            vetorDobro[i] = vetorOriginal[i] * 2;
+        }
+
+         System.out.println("Valores do segundo vetor (dobro dos valores do primeiro vetor):");
+         for (int i = 0; i < tamanho; i++) {
+            System.out.println("Posição " + i + ": " + vetorDobro[i]);
+        }
+
+        scanner.close();
+    }
+}
