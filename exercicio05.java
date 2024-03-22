9fc77ba1545aab269aed9331e3de889959d1bdae
Mayara <mayara.wojceikovski@gmail.com>
2024-03-22 17:59:51 -0300

    exercicio05

---
+++ exercicio05.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class exercicio05{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] vetor = new int[5];
+        System.out.println("Digite 5 números:");
+        for (int i = 0; i < 5; i++) {
+            vetor[i] = scanner.nextInt();
+        }
+
+        System.out.print("Digite um número n: ");
+        int n = scanner.nextInt();
+        int vezes = 0;
+        for (int i = 0; i < 5; i++) {
+            if (vetor[i] == n) {
+                vezes++;
+            }
+        }
+        System.out.println("O número " + n + " aparece " + vezes + " vezes no vetor.");
+
+        scanner.close();
+    }
+}
